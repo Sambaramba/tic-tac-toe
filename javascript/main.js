cd9fb8897e600cb6d4a67f46cebdf7aeafa160f4
Sambaramba <samuelharrison19@tutanota.com>
2026-02-10 10:51:34 +0000

    feat/refactor: add code to select available cells for square selection and change prompt and regEx

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,8 @@
 const getGameBoard = (function () {
+    //change to empty array?
     let grid = new Array(9).fill("");
+
+    const getGridCell = () => {}
     return {grid};
 })();
 
@@ -62,25 +65,70 @@ const player2 = createPlayer("player2");
 //then if false get all indexs that are empty
 //add numbers to new array? or string of numbers? then change pattern and prompt to only allow those numbers?
 function playerTurn(player) {
+    //if none free squares display game over with winner - call win condition func
+    //loop through gameboard and find all empty indexes
+    // get those numbers
+    //add to prompt and pattern
 
+    
+    
     let squareSelection;
+    
+    
+    
+    //-------DOESN'T ADD 0 INDEX TO EMPTY CELLS----------------
+
+    //is it because zero is falsy value?
 
     const selectGameboardSquare = () => {
-        const numberInRangeCheck = /^[0-8]$/;
+        let arrayOfEmptyCells = [];
+        let emptyValue = "";
+        getGameBoard.grid.forEach((element, index) => {
+            if(element === emptyValue) arrayOfEmptyCells.push(index);
+                console.log(index);
+        })
+        // console.log(arrayOfEmptyCells);
+        let emptyCells = Number(arrayOfEmptyCells.join(""));
+        console.log(typeof emptyCells);
+        console.log(emptyCells);
+        // let numberInRangeCheck = /^[0-8]$/;
+        // console.log(emptyCells);
+        let numberInRangeCheck = new RegExp (`^[${emptyCells}]$`);
+        console.log(numberInRangeCheck);
+        // const containsNothing = (currentValue) => currentValue === "";
+        // if (getGameBoard.grid.every(containsNothing)) {
+        // console.log("all contain nothing");
+        // }
+        // getGameBoard.grid
         do {
-           squareSelection = prompt("Enter a number between 0-8 to place piece");
+            squareSelection = prompt(`Please choose one of these numbers ${emptyCells} to select grid square`);
         } while (!numberInRangeCheck.test(squareSelection) || squareSelection === null);
+        //numberInRange - squareSelection value
+        //remove from prompt
         
        return squareSelection;
     }
-
-
+    console.log(squareSelection);
+    //so loop through gameboard grid
+    //do i put that method/function in gameboard or turn func?
+    //use square selection as index to search array
+    //check if empty and add if so
+    //else ask for new square selection
+    // getGameBoard.getGridCell(squareSelection);
+    console.log(getGameBoard.grid);
+    
+    // for (let cell of getGameBoard.grid) {
+    //     if (containsNothing(cell))
+    //     console.log(cell);
+    // }
+    
     
 
     return {selectGameboardSquare}
 }
 
 playerTurn(player1).selectGameboardSquare();
+playerTurn(player1)
 
 // if(playe)
 
@@ -117,5 +165,10 @@ function playGame() {
 //so could only allow 9x playerTurns
 //then from turn 5 onwards start checking grid for 3 in row;
 function checkWinCondition() {
+    //swap this to containsSomething and value !==
+    // const containsSomething = (currentValue) => currentValue !== "";
+    // if (getGameBoard.grid.every(containsSomething)) {
+    //     console.log("all contain something");
+    // }
     
 }
